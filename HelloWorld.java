388cfd6639d92978775338471b2113e4cbc1a58c
Teja-x <tejanayani668@gmail.com>
2025-06-04 19:36:31 +0530

    Update HelloWorld.java with a test comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,7 @@
-public class HelloWorld{ public static void main(String args[]){ System.out.println(Hello World! This is a practice git-java file.n);}}
+public class HelloWorld {
+    public static void main(String args[])
+    { 
+        //This is test comment
+        System.out.println("Hello World! This is a practice git-java file.\n");
+    }
+}
