af2708f1e05b63a99f7ca51b1a86b092ac1708a8
Teja-x <tejanayani668@gmail.com>
2025-06-18 18:46:46 +0530

    commit to understand git stash

--- AreaOfSquare.java
+++ AreaOfSquare.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class AreaOfSquare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side of the square:");
         int side = sc.nextInt();
         System.out.println("Area of the Square is " + (side * side));
     }
